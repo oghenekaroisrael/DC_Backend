af17156ac032c0caa0d4c72595fd09569bdce4ff
oghenekaro brume israel <oghenekaroisrael089@gmail.com>
2022-08-02 11:09:38 +0100

    changed base url

--- src/app/redux/helpers/api.js
+++ src/app/redux/helpers/api.js
@@ -1,6 +1,6 @@
 export const endpoints = {
     // API_HOME: "https://deliveryconnectproject-78386.uc.r.appspot.com",
-    API_HOME: "https://delivery-connect-backend.herokuapp.com/api/v1",
+    API_HOME: "https://delivery-connect-api.herokuapp.com/api/v1",
     // API_HOME: "http://192.168.0.155:3002",
     // API_HOME: "http://192.168.43.207:3002",
 }
