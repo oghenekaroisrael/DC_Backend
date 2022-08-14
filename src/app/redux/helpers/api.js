3e3e5ab80c2ac084d1c54b54db3c498f320adb81
oghenekaro brume israel <oghenekaroisrael089@gmail.com>
2022-08-14 20:21:38 +0100

    fixed some issues

--- src/app/redux/helpers/api.js
+++ src/app/redux/helpers/api.js
@@ -1,8 +1,8 @@
 export const endpoints = {
     // API_HOME: "https://deliveryconnectproject-78386.uc.r.appspot.com",
-    // API_HOME: "https://delivery-connect-api.herokuapp.com/api/v1",
+    API_HOME: "https://delivery-connect-api.herokuapp.com/api/v1",
     // API_HOME: "http://192.168.0.155:3002",
-    API_HOME: "http://localhost:5000/api/v1",
+    // API_HOME: "http://localhost:5000/api/v1",
 }
 
 export function getHeaders(auth = false) {
